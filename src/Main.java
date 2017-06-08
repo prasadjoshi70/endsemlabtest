d47b8f164bab2a607e3d078d51a3a67663264851
prasadjoshi70 <prasadjoshi70@gmail.com>
2017-06-08 15:44:16 +0530

    Changes made
    Signed-off-by: prasadjoshi70 <prasadjoshi70@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Evm t=new Evm(1,"Thivim");
+		Evm t=new Evm(1,"Thivim"); //Constinuency 1
 		Candidate c1=new Candidate(t,"Prasad");
 		Candidate c5=new Candidate(t,"Vinay");
 		
-		Evm m=new Evm(2,"Mapusa");
+		Evm m=new Evm(2,"Mapusa"); //Constinuency 2
 		Candidate c2=new Candidate(m,"babush");
 		Candidate c3=new Candidate(m,"Shraddha");
 		Candidate c4=new Candidate(m,"Vinod");
@@ -38,10 +38,27 @@ public class Main {
 		System.out.println("Blue button pressed!! Your vote has been recorded");
 		
 		//voter 6 has voted
-		Voter v6=new Voter(104,c3);
+		Voter v6=new Voter(104,c2);
+		c2.incrementVoteCount();
+		System.out.println("Blue button pressed!! Your vote has been recorded");
+		
+		//voter 7 has voted
+		Voter v7=new Voter(104,c3);
 		c3.incrementVoteCount();
 		System.out.println("Blue button pressed!! Your vote has been recorded");
+				
+		
 		System.out.println("************************");
+		System.out.println("");
+		
+		System.out.println("Total number of votes polled during the poll is "+Voter.counter());
+		System.out.println("************************");
+		System.out.println("");
+		
+		System.out.println("After closing poll:");
+		System.out.print(Evm.closepoll());
+		System.out.println("************************");
+		System.out.println("");
 		
 		System.out.println(c1.getName()+" got "+c1.getNumberOfVotes()+" number of votes from "+ t.getConstinuency());
 		System.out.println(c5.getName()+" got "+c5.getNumberOfVotes()+" number of votes from "+ t.getConstinuency());
@@ -50,16 +67,12 @@ public class Main {
 		System.out.println(c2.getName()+" got "+c2.getNumberOfVotes()+" number of votes from "+ m.getConstinuency());
 		System.out.println(c4.getName()+" got "+c4.getNumberOfVotes()+" number of votes from "+ m.getConstinuency());
 		System.out.println("************************");
-		System.out.println("Total number of votes polled during the poll is "+Voter.counter());
-		System.out.println("************************");
+		System.out.println("");
 		
 		Voter.resetCounter();
 		System.out.println("After reseting counter:");
 		System.out.println("Total number of votes polled during the poll is "+Voter.counter());
-		System.out.println("************************");
 		
-		System.out.println("After closing poll:");
-		System.out.print(Evm.closepoll());
 		
 	}
 
